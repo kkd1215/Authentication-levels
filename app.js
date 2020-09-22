2937879a99d8003a97cf16355ac7ea4bd63a0079
kkd1215 <karandewani98@gmail.com>
2020-09-22 20:05:34 +0530

    complete level 5 Authentication

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session  = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+
 
 const app = express();
 
@@ -14,67 +17,99 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect('mongodb://localhost:27017/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model('User', userSchema);
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
+passport.use(User.createStrategy());
 
-app.route('/login')
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+///////////////////////////////////////////// Home Route //////////////////////////////////////////////////////////////////
+app.route('/')
   .get((req, res) => {
-    res.render('login');
-  })
-
-  .post((req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email : username}, (err, foundUser) => {
-      if(err){
-        res.send(err);
-      }else {
-        if(foundUser) {
-          bcrypt.compare(password, foundUser.password, (err, result) => {
-            if(result === true) {
-                res.render('secrets');
-              }
-          });
-
-        }
-      }
-    });
-
+    res.render('home');
   });
 
+//////////////////////////////////////////// Secret Route ////////////////////////////////////////////////////////////////
+
+app.route('/secrets')
+  .get((req, res) => {
+    if(req.isAuthenticated()) {
+      res.render('secrets')
+    }else {
+      res.redirect('/login')
+    }
+  });
+
+//////////////////////////////////////////// Register Route //////////////////////////////////////////////////////////////
 app.route('/register')
   .get((req, res) => {
     res.render('register');
   })
 
   .post((req, res) => {
-
-    bcrypt.hash(req.body.password,saltRounds, (err, hash) => {
-      const newUser = new User({
-        email: req.body.username,
-        password: hash
-      });
-
-      newUser.save(err => {
-        if (err) {
-          console.log(err)
-        } else {
-          res.render('secrets');
-        }
-      });
+    User.register({username: req.body.username}, req.body.password, (err, user) => {
+      if(err) {
+        console.log(err);
+        res.redirect('/register');
+      }else {
+        passport.autenticate('local')(req, res, () => {
+          res.redirect('/secrets');
+        });
+      }
     });
+  });
+///////////////////////////////////////////// Login Route ////////////////////////////////////////////////////////////////
+app.route('/login')
+  .get((req, res) => {
+    res.render('login');
+  })
+
+  .post((req, res) => {
+    const user = new User({
+      username:req.body.username,
+      password:req.body.password
+    });
+
+    req.login(user, err => {
+      if(err){
+        console.log(err);
+      }else {
+        passport.autenticate('local')(req, res, () => {
+          res.redirect('/secrets');
+        });
+      }
+    });
+
+  });
+
+/////////////////////////////////////////// Logout Route //////////////////////////////////////////////////////////////////
+
+app.route('/logout')
+  .get((req, res) => {
+    res.redirect('/');
   });
 
 app.listen(3000, () => {
