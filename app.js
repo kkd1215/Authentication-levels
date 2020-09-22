81ef74811544f4e14cab792f4bcbfd0258c5785a
kkd1215 <karandewani98@gmail.com>
2020-09-22 18:14:57 +0530

    complete level 4 Authentication

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -34,16 +35,19 @@ app.route('/login')
 
   .post((req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email : username}, (err, foundUser) => {
       if(err){
         res.send(err);
       }else {
         if(foundUser) {
-          if(foundUser.password === password){
-            res.render('secrets');
-          }
+          bcrypt.compare(password, foundUser.password, (err, result) => {
+            if(result === true) {
+                res.render('secrets');
+              }
+          });
+
         }
       }
     });
@@ -57,17 +61,19 @@ app.route('/register')
 
   .post((req, res) => {
 
-    const newUser = new User({
-      email: req.body.username,
-      password: md5(req.body.password)
-    });
+    bcrypt.hash(req.body.password,saltRounds, (err, hash) => {
+      const newUser = new User({
+        email: req.body.username,
+        password: hash
+      });
 
-    newUser.save(err => {
-      if (err) {
-        console.log(err)
-      } else {
-        res.render('secrets');
-      }
+      newUser.save(err => {
+        if (err) {
+          console.log(err)
+        } else {
+          res.render('secrets');
+        }
+      });
     });
   });
 
